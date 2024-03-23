e7fc643efbfd163006efd8a1cd7c73b89bc62d6d
ManuVleurickVUB <Manu.Gaston.R.Vleurick@vub.be>
2024-03-23 23:06:52 +0100

    Functional NavBar without working links

--- test_nextjs_school/components/NavBar.tsx
+++ test_nextjs_school/components/NavBar.tsx
@@ -1,19 +1,28 @@
 import Link from "next/link"
 import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
 
 const NavBar = () => {
     return (
-      <nav className="flex flex-row border-2 border-red-500">
+      <div className="flex flex-row gap-28 border-2 border-yellow-500">
+        {/* LOGO */}
+        <div className="self-center">
+          <Link href="/">
+            <Image src="/nav_logo.jpg" alt="logo" width={80} height={80} />
+          </Link>
+        </div>
 
-
-        <ul>
-          {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key} className="font-bold cursor-pointer px-5">
-              {link.label}
-            </Link>
-          ))}
-        </ul>
-      </nav>
+        <nav className="flex flex-row flex-nowrap border-2 border-red-500">
+          <ul className="flex flew-row self-center">
+            {NAV_LINKS.map((link) => (
+              <Link href={link.href} key={link.key} className="font-bold cursor-pointer px-5">
+                <Image src={link.icon} alt={link.label} width={48} height={48} />
+                <label>{link.label}</label>
+              </Link>
+            ))}
+          </ul>
+        </nav>
+      </div>
     )
   }
   
--- test_nextjs_school/constants/index.ts
+++ test_nextjs_school/constants/index.ts
@@ -1,5 +1,5 @@
 
 export const NAV_LINKS = [
-    { href: '/', key: 'graph', label: 'Graph' },
-    { href: '/', key: 'image', label: 'Image' },
+    { href: '/', key: 'graph', label: 'Graph', icon: '/nav_graph_icon.png' },
+    { href: '/', key: 'image', label: 'Image', icon: '/nav_image_icon.png' },
   ];
